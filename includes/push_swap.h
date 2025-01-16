7363344191f9b52a8caff49cb82720a10caf559a
sboukiou <sboukiou@student.1337.ma>
2025-01-16 17:23:29 +0100

    includes adding lists.h for linkedlists functions

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -18,12 +18,10 @@
 #include "./common.h"
 #include "./libft.h"
 #include "./error.h"
+#include "./lists.h"
 
 
 /*Stack dsa functions*/
-t_node	*stack_create_node(int value, t_node *prev);
-t_stack	*stack_push_back(t_stack *stack, t_node *node);
-t_stack	*stack_push_front(t_stack *stack, t_node *node);
 void	stack_free(t_stack *stack_data);
 void	stack_print(t_stack *stack_data);
 t_stack	*stack_last(t_stack *stack_data);
