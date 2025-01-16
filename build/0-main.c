44fd03c9234ab8b18a90146c36b9746976f7da93
sboukiou <sboukiou@student.1337.ma>
2025-01-16 16:56:49 +0100

    builds

--- build/0-main.c
+++ build/0-main.c
@@ -4,16 +4,40 @@
 
 int main(int ac, char **av)
 {
-	t_stack	*numbers;
+	t_stack	*stack_a;
+	t_stack	*stack_b;
 	/*t_node	*temp;*/
 	if(ac < 2)
 		return (0);
 	error_check(av);
-	numbers = tokenize_input(av);
-	if (!numbers)
-		printf("Nothing returned from tokenizer, Leaving ...\n");
-	stack_print(numbers);
-	stack_free(numbers);
+	stack_a = tokenize_input(av);
+	error_duplicate(stack_a);
+	stack_b = (t_stack *)malloc(sizeof(t_stack));
+	if (!stack_a || !stack_b)
+		printf("Nothing returned from tokenizer, Leaving ...\n"), exit(0);
+
+	stack_b->top = NULL;
+	stack_b->bottom = NULL;
+	/*----------------------------------------------------*/
+	printf("Stack initialized\n-------------\n");
+	stack_print(stack_a);
+	/*----------------------------------------------------*/
+
+	/* rra works fine so far */
+	/*----------------------------------------------------*/
+	printf("aplying rra ...\n-------------\n");
+	stack_print(stack_a);
+	sorter(stack_a, stack_b);
+
+	/*----------------------------------------------------*/
+
+	/*----------------------------------------------------*/
+	/*printf("Stack sorted\n-------------\n");*/
+	/*stack_print(stack_a);*/
+
+	/*----------------------------------------------------*/
+	stack_free(stack_a);
+	stack_free(stack_b);
 	return (0);
 
 }
