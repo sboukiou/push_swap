a5ebe3c0c7206db8601844676d6e3ececa16a43c
sboukiou <sboukiou@student.1337.ma>
2025-01-20 09:40:02 +0100

    Main testing the parsing part, and mmvnt funcs

--- build/0-main.c
+++ build/0-main.c
@@ -4,16 +4,22 @@
 
 int main(int ac, char **av)
 {
-	t_stack	*numbers;
+	t_stack	*stack;
 	/*t_node	*temp;*/
 	if(ac < 2)
 		return (0);
 	error_check(av);
-	numbers = tokenize_input(av);
-	if (!numbers)
+	stack = tokenize_input(av);
+	if (!stack)
 		printf("Nothing returned from tokenizer, Leaving ...\n");
-	stack_print(numbers);
-	stack_free(numbers);
+	stack_print(stack);
+	rrab(stack);
+	stack_print(stack);
+	rrab(stack);
+	rrab(stack);
+	printf("after second rrab\n");
+	stack_print(stack);
+	stack_free(stack);
 	return (0);
 
 }
