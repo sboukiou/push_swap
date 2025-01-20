1eb434f4efb07822ca085321b703f5534dbd86ea
sboukiou <sboukiou@student.1337.ma>
2025-01-20 15:36:34 +0100

    Generator added

---
+++ build/generator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+
+int main(void)
+{
+	printf("Hello, World!\n");
+	printf("RAND MAX is %d\n", RAND_MAX);
+	printf("INT MAX --> %d\n", INT_MAX);
+	return (0);
+}
