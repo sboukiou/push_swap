8fba5954c6c2546595e0210b94fac7685cf2b9d1
sboukiou <sboukiou@student.1337.ma>
2025-01-20 15:49:38 +0100

    Generator added

--- build/generator.c
+++ build/generator.c
@@ -2,11 +2,40 @@
 #include <stdlib.h>
 #include <limits.h>
 
-
-int main(void)
+int	not_in(int *arr, int value, int size)
 {
-	printf("Hello, World!\n");
-	printf("RAND MAX is %d\n", RAND_MAX);
-	printf("INT MAX --> %d\n", INT_MAX);
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] == value)
+			return (0);
+	}
+	return (1);
+}
+
+int main(int ac, char **av)
+{
+	int	value;
+	int size;
+	int	*chunks;
+
+	printf("Generating value ...\n");
+	size = 10;
+	if (ac > 1)
+		size = atoi(av[1]);
+	chunks = (int *)calloc(size, sizeof(int));
+	if (!chunks)
+		return (0);
+
+	for (int i = 0; i<= size; i++)
+	{
+		value = rand();
+		while (not_in(chunks, value, size) == 0)
+			value = rand();
+		chunks[i] = value;
+	}
+	for (int i = 0; i < size; i++)
+		printf("%d ", chunks[i]);
+	printf("\n");
+	free(chunks);
 	return (0);
 }
