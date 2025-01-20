4b20fc22de3d16f53264cf72307514e84ce61ec6
sboukiou <sboukiou@student.1337.ma>
2025-01-20 12:11:10 +0100

    Main for testing

--- build/0-main.c
+++ build/0-main.c
@@ -5,6 +5,7 @@
 int main(int ac, char **av)
 {
 	t_stack	*stack;
+	t_stack	*stack_b;
 	/*t_node	*temp;*/
 	if(ac < 2)
 		return (0);
@@ -12,13 +13,19 @@ int main(int ac, char **av)
 	stack = tokenize_input(av);
 	if (!stack)
 		printf("Nothing returned from tokenizer, Leaving ...\n");
-	stack_print(stack);
-	rrab(stack);
-	stack_print(stack);
-	rrab(stack);
-	rrab(stack);
-	printf("after second rrab\n");
-	stack_print(stack);
+	error_stack_dup(stack);
+	/*stack_print(stack);*/
+	/*rrab(stack);*/
+	/*rrab(stack);*/
+	/*printf("after second rrab\n");*/
+	/*stack_print(stack);*/
+	/*printf("After sorting the stack\n");*/
+	stack_b = (t_stack *)malloc(sizeof(t_stack));
+	stack_b->top = NULL;
+	stack_b->bottom = NULL;
+	/*stack_sort(stack);*/
+	sorter(stack, stack_b);
+	/*stack_print(stack);*/
 	stack_free(stack);
 	return (0);
 
