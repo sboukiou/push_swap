2c85198252a29db0545bde7bf8f9617261ec4b98
sboukiou <sboukiou@student.1337.ma>
2025-01-23 00:08:46 +0100

    Addin new type of ft_atoi, Using the long typ in return

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -32,11 +32,11 @@ static int	is_delim(char c)
 	* @nptr: Alphanum string to convert
 	* Return: An int depending on the nptr
 */
-long	ft_atoi(const char *nptr)
+int	ft_atoi(const char *nptr)
 {
-	int		count;
-	int		sign;
-	long	num;
+	int				count;
+	int				sign;
+	unsigned int	num;
 
 	count = 0;
 	while (is_delim(nptr[count]) && nptr[count])
