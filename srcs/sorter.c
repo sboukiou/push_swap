50414d1d67e0a9308172feb066d1ca00c8883c29
sboukiou <sboukiou@student.1337.ma>
2025-01-23 03:17:59 +0100

    Implementing functions to sort sequences of 3,4,5 numbers

--- srcs/sorter.c
+++ srcs/sorter.c
@@ -57,6 +57,67 @@ void	sort_size_three(t_stack *stack_a)
 	}
 }
 
+void	sort_size_five(t_stack *stack_a, t_stack *stack_b)
+{
+	t_stack	*ref;
+
+	ref = stack_duplicate(stack_a);
+	if (!ref)
+		return ;
+	stack_sort(ref);
+
+	while (stack_a->top->value != ref->top->value)
+	{
+		if (stack_index_of(stack_a, ref->top->value) > stack_size(stack_a) / 2)
+		{
+			while (stack_a->top->value != ref->top->value)
+			{
+				printf("rra\n");
+				rrab(stack_a);
+			}
+		}
+		else
+		{
+			while (stack_a->top->value != ref->top->value)
+			{
+				printf("ra\n");
+				rab(stack_a);
+			}
+		}
+	}
+	printf("pb\n");
+	pab(stack_a, stack_b);
+	if (stack_size(stack_a) == 4)
+	{
+		while (stack_a->top->value != ref->top->next->value)
+		{
+			if (stack_index_of(stack_a, ref->top->next->value) > stack_size(stack_a) / 2)
+			{
+				while (stack_a->top->value != ref->top->next->value)
+				{
+					printf("rra\n");
+					rrab(stack_a);
+				}
+			}
+			else
+			{
+				while (stack_a->top->value != ref->top->next->value)
+				{
+					printf("ra\n");
+					rab(stack_a);
+				}
+			}
+		}
+		printf("pb\n");
+		pab(stack_a, stack_b);
+	}
+	sort_size_three(stack_a);
+	pab(stack_b, stack_a);
+	pab(stack_b, stack_a);
+	printf("pa\n");
+	printf("pa\n");
+}
+
 void	sorter(t_stack *stack_a, t_stack *stack_b)
 {
 	(void)stack_a;
@@ -66,9 +127,9 @@ void	sorter(t_stack *stack_a, t_stack *stack_b)
 
 	if (stack_size(stack_a) == 3)
 		return (sort_size_three(stack_a));
+	if (stack_size(stack_a) == 4 || stack_size(stack_a) == 5)
+		return (sort_size_five(stack_a, stack_b));
 	ref = stack_duplicate(stack_a);
-	if (!ref)
-		return ;
 	stack_sort(ref);
 	temp = ref->top;
 	while (temp)
