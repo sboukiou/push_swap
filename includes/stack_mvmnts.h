35c8d0de532fb69a4cafd41602fd9633681f4409
sboukiou <sboukiou@student.1337.ma>
2025-01-23 04:17:45 +0100

    Addin new header and source for stack helper and advanced mvmnt functions

---
+++ includes/stack_mvmnts.h
@@ -0,0 +1,9 @@
+#ifndef STACK_MVMNTS_H
+#define STACK_MVMNTS_H
+#include "./common.h"
+void	find_and_push(t_stack *stack_a, t_stack *stack_b, int value);
+void	sab(t_stack	*stack);
+void	rab(t_stack	*stack);
+void	rrab(t_stack	*stack);
+void	pab(t_stack *a, t_stack *b);
+#endif
