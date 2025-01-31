c795025d080d8acc0f18cf98c9f311ca14b06dde
sboukiou <sboukiou@student.1337.ma>
2025-01-31 01:21:43 +0100

    Fixing norme for includes

--- includes/stack_mvmnts.h
+++ includes/stack_mvmnts.h
@@ -6,7 +6,7 @@
 /*   By: sboukiou <sboukiou@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 11:17:51 by sboukiou          #+#    #+#             */
-/*   Updated: 2025/01/30 11:18:06 by sboukiou         ###   ########.fr       */
+/*   Updated: 2025/01/30 11:41:27 by sboukiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "./common.h"
 
 void	find_and_push(t_stack *stack_a, t_stack *stack_b, int value);
+void	find_and_push_b(t_stack *stack_a, t_stack *stack_b, int value);
 void	sab(t_stack	*stack, char *str);
 void	rab(t_stack	*stack, char *str);
 void	rrab(t_stack	*stack, char *str);
