75d54f092143298b98540c0d4542996c35bbdfa9
sboukiou <sboukiou@student.1337.ma>
2025-02-01 20:50:42 +0100

    Implemented all actions required in actions.c

--- actions.c
+++ actions.c
@@ -10,6 +10,43 @@ void	pab(t_stack *stack_a, t_stack *stack_b, char *str)
 	if (!stack_a || !stack_b || !stack_a->top)
 		return ;
 	temp = stack_a->top;
-	/*stack_remove(stack_a, stack_a->top);*/
-	/*stack_push_front(stack_b, temp);*/
+	stack_remove(stack_a, stack_a->top);
+	stack_push_front(stack_b, temp);
+}
+
+void	sab(t_stack *stack, char *str)
+{
+	t_node	*temp;
+
+	if (str)
+		write(STDOUT_FILENO, str, ft_strlen(str));
+	temp = stack->top;
+	stack->top = temp->next;
+	stack->top->next = temp;
+}
+
+void	rab(t_stack *stack, char *str)
+{
+	t_node	*temp;
+
+	if (!stack || !stack->top)
+		return ;
+	if (str)
+		write(STDOUT_FILENO, str, ft_strlen(str));
+	temp = stack->top;
+	stack_remove(stack, temp);
+	stack_push_back(stack, temp);
+}
+
+void	rrab(t_stack *stack, char *str)
+{
+	t_node	*temp;
+
+	if (!stack || !stack->top)
+		return ;
+	if (str)
+		write(STDOUT_FILENO, str, ft_strlen(str));
+	temp = stack->bottom;
+	stack_remove(stack, temp);
+	stack_push_front(stack, temp);
 }
