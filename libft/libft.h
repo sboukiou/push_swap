5a5bf9aea5881d1b409923af292c6c489da93558
sboukiou <sboukiou@student.1337.ma>
2025-02-01 20:23:45 +0100

    libft added more needed sources

--- libft/libft.h
+++ libft/libft.h
@@ -26,4 +26,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, unsigned int n);
 int		ft_atoi(const char *nptr);
 char	**ft_split(const char *s, char c);
+void	*ft_calloc(size_t nmemb, size_t size);
+void	*ft_memset(void *s, int c, size_t n);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 #endif
