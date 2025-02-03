c9f25d06de29a41efbed425a1270bfd256b7b346
sboukiou <sboukiou@student.1337.ma>
2025-02-03 09:53:51 +0100

    Addin condition in main.c , to quit if no args were given

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sboukiou <sboukiou@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 11:24:57 by sboukiou          #+#    #+#             */
-/*   Updated: 2025/02/02 11:25:26 by sboukiou         ###   ########.fr       */
+/*   Updated: 2025/02/03 09:53:32 by sboukiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,8 @@ int	main(int ac, char **av)
 	t_stack	*stack_a;
 	t_stack	*stack_b;
 
-	(void)ac;
+	if (ac == 1)
+		return (0);
 	stack_a = parser(av);
 	stack_b = stack_new(NULL, NULL);
 	if (!stack_a || !stack_b)
