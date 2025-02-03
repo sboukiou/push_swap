d7dc7ac89f1c51d7870167bce62202ab2e2772be
sboukiou <sboukiou@student.1337.ma>
2025-02-03 15:48:12 +0100

    In main fixed leaks, + norme edit in main_bonus

--- main_bonus.c
+++ main_bonus.c
@@ -1,87 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   bonus.c                                            :+:      :+:    :+:   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sboukiou <sboukiou@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 09:52:57 by sboukiou          #+#    #+#             */
-/*   Updated: 2025/02/03 10:23:03 by sboukiou         ###   ########.fr       */
+/*   Updated: 2025/02/03 15:44:46 by sboukiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "./push_swap.h"
-
-void	quit(t_stack *stack_a, t_stack *stack_b)
-{
-	stack_free(stack_a);
-	stack_free(stack_b);
-	write(STDOUT_FILENO,"Error\n", 6);
-	exit(0);
-}
-
-static int	move_isvalide(char *move)
-{
-	if (!move)
-		return (0);
-	if (!ft_strncmp(move, "rra\n", 4))
-		return (1);
-	if (!ft_strncmp(move, "rrb\n", 4))
-		return (1);
-	if (!ft_strncmp(move, "ra\n", 3))
-		return (1);
-	if (!ft_strncmp(move, "rb\n", 3))
-		return (1);
-	if (!ft_strncmp(move, "rr\n", 3))
-		return (1);
-	if (!ft_strncmp(move, "rrr\n", 4))
-		return (1);
-	if (!ft_strncmp(move, "sa\n", 3))
-		return (1);
-	if (!ft_strncmp(move, "ss\n", 3))
-		return (1);
-	if (!ft_strncmp(move, "sb\n", 3))
-		return (1);
-	if (!ft_strncmp(move, "pa\n", 3))
-		return (1);
-	if (!ft_strncmp(move, "pb\n", 3))
-		return (1);
-	return (0);
-}
-
-static void	apply_move(t_stack *stack_a, t_stack *stack_b, char *move)
-{
-	if (!move)
-		return ;
-	if (!ft_strncmp(move, "rra\n", 4))
-		rrab(stack_a, NULL);
-	else if (!ft_strncmp(move, "rrb\n", 4))
-		rrab(stack_b, NULL);
-	else if (!ft_strncmp(move, "ra\n", 3))
-		rab(stack_a, NULL);
-	else if (!ft_strncmp(move, "rb\n", 3))
-		rab(stack_b, NULL);
-	else if (!ft_strncmp(move, "rr\n", 3))
-		return (rab(stack_b, NULL), rab(stack_a, NULL));
-	else if (!ft_strncmp(move, "rrr\n", 4))
-		return (rrab(stack_b,NULL), rrab(stack_a,NULL));
-	else if (!ft_strncmp(move, "sa\n", 3))
-			sab(stack_a, NULL);
-	else if (!ft_strncmp(move, "sb\n", 3))
-			sab(stack_b, NULL);
-	else if (!ft_strncmp(move, "ss\n", 3))
-			return (sab(stack_a, NULL), sab(stack_b, NULL));
-	else if (!ft_strncmp(move, "pa\n", 3))
-			pab(stack_b, stack_a, NULL);
-	else if (!ft_strncmp(move, "pb\n", 3))
-			pab(stack_a, stack_b, NULL);
-}
+#include "./push_swap_bonus.h"
 
 static char	*get_line(int fd)
 {
-	char	(*line), (byte);
-	int		(idx), (byte_read);
+	char	*line;
+	char	byte;
+	int		idx;
+	int		byte_read;
 
 	if (fd < 0 || fd > 1024)
 		return (NULL);
@@ -103,10 +40,9 @@ static char	*get_line(int fd)
 	return (line);
 }
 
-
 void	apply_moves(t_stack *stack_a, t_stack *stack_b, int fd)
 {
-	char	(*move);
+	char	*move;
 
 	move = get_line(fd);
 	while (move)
@@ -124,21 +60,6 @@ void	apply_moves(t_stack *stack_a, t_stack *stack_b, int fd)
 		free(move);
 		move = get_line(fd);
 	}
-}
-
-void stack_print(t_stack *stack)
-{
-	t_node *temp;
-
-	if (!stack)
-		return ;
-	temp = stack->top;
-	while (temp)
-	{
-		printf(" %d -> ", temp->value);
-		temp = temp->next;
-	}
-	printf("\n");
 }
 
 int main(int ac, char **av)
